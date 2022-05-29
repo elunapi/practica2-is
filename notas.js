6f0d6080cc28b5a444209e28d94e93bfaaaed933
Erick Luna <elunapi@ciencias.unam.mx>
2022-05-29 11:46:02 -0500

    Se implementó la mayor parte de la vista

---
+++ notas.js
@@ -0,0 +1,8 @@
+function guardarNota(){
+    var nota = document.createElement("div");
+    nota.className = "container round shadow";
+    nota.innerHTML = "hola";
+    nota.style = "margin-top: 5%";
+    document.getElementById("listaNotas").appendChild(nota);
+    
+}
