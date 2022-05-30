30704bebb81252e5447fa5904d8d28461ea5afca
Enrique Alcántara <enrique_eduaa@ciencias.unam.mx>
2022-05-29 21:49:11 -0500

    Implementado guardar notas, borrar notas y limpiar texto.

--- notas.js
+++ notas.js
@@ -1,8 +1,42 @@
 function guardarNota(){
     var nota = document.createElement("div");
-    nota.className = "container round shadow";
-    nota.innerHTML = "hola";
+    var titulo = document.createElement("div");
+    var texto =  document.createElement("div");
+    var boton = document.createElement("button");
+
+    nota.className = "column container round shadow";
+
+    titulo.innerHTML = document.getElementById("tituloNota").value;
+    texto.innerHTML = document.getElementById("textoNota").value;
+    boton.innerHTML = "Borrar Nota";
+
     nota.style = "margin-top: 5%";
-    document.getElementById("listaNotas").appendChild(nota);
+
+    if(titulo.innerHTML != '' && texto.innerHTML != '') {
+        nota.appendChild(titulo);
+        nota.appendChild(texto);
+
+        boton.onclick = function(){borrarNota(this);}
+
+        nota.appendChild(boton);
+        document.getElementById("listaNotas").appendChild(nota);
+
+
+
+        limpiarTexto();
+    }
     
+}
+
+function limpiarTexto(){
+    document.getElementById("tituloNota").value = '';
+
+    document.getElementById("textoNota").value = '';
+}
+
+function borrarNota(boton){
+    var nota = boton.parentNode;
+
+    nota.parentNode.removeChild(nota);
+
 }
