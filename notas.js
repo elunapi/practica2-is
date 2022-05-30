a11d48fb92426f51d89132262b1480d467a6309e
JazminGarciaG <miingarcia@ciencias.unam.mx>
2022-05-29 22:59:20 -0500

    Se agregaron las funciones de cambiar color

--- notas.js
+++ notas.js
@@ -1,9 +1,13 @@
+var colorTitulo = 0;
+var colorTexto = 0;
+let colores = ["black", "red", "orange", "darkgoldenrod", "green", "blue", "indigo", "violet", "brown", "grey"];
+
 function guardarNota(){
     var nota = document.createElement("div");
     var titulo = document.createElement("div");
     var texto =  document.createElement("div");
     var boton = document.createElement("button");
-
+    
     nota.className = "column container round shadow";
 
     titulo.innerHTML = document.getElementById("tituloNota").value;
@@ -13,6 +17,10 @@ function guardarNota(){
     nota.style = "margin-top: 5%";
 
     if(titulo.innerHTML != '' && texto.innerHTML != '') {
+
+        titulo.style = "color: "+colores[colorTitulo] + "; font-size: 30px";
+        texto.style = "color: "+colores[colorTexto];
+        boton.style = "margin-top: 10px"
         nota.appendChild(titulo);
         nota.appendChild(texto);
 
@@ -39,4 +47,16 @@ function borrarNota(boton){
 
     nota.parentNode.removeChild(nota);
 
+}
+
+function cambiarColorTitulo(){
+    colorTitulo = (colorTitulo + 1) % 10;
+
+    document.getElementById("botonTitulo").style = "background-color: " + colores[colorTitulo];
+}
+
+function cambiarColorTexto(){
+    colorTexto = (colorTexto + 1) % 10;
+
+    document.getElementById("botonTexto").style = "background-color: " + colores[colorTexto];
 }
